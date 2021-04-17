868d1d76f1e2960a9a15cd33acff92c5b07a8a46
bellaappel <bellaappel0@gmail.com>
2021-04-17 13:49:45 -0400

    index working and delete button added but not working

--- src/BallIndex.js
+++ src/BallIndex.js
@@ -1,26 +1,48 @@
-    class BallIndex {
-        constructor(listBallObj) {
-            this.listBallObj = listBallObj;
-        }
+
     
-       static renderBalls = (ball) => {
-        let ballName = document.createElement("h3")
-        ballName.innerText = ball.name
+      function renderBalls(balls) {
+        balls.forEach(ball => {
+              
+          
+            let ballName = document.createElement("h3")
+            ballName.innerText = ball.name
 
-        let creatorName = document.createElement("h3")
-        creatorName.innerText = ball.creator.name
+            let creatorName = document.createElement("h3")
+            creatorName.innerText = ball.creator.name
 
-        let useBtn = document.createElement('button')
-        useBtn.setAttribute('class', 'use-ball-btn')
-        useBtn.setAttribute('id', ball.id)
-        useBtn.innerText = "Select This Ball"
-        // ADD EVENT LISTENER ON THIS BUTTON
+            let useBtn = document.createElement('button')
+            useBtn.setAttribute('class', 'use-ball-btn')
+            useBtn.setAttribute('id', ball.id)
+            useBtn.innerText = "Select This Ball"
+            useBtn.addEventListener("click", playBall, {once:true})
+            // ADD EVENT LISTENER ON THIS BUTTON  
 
-        let divCard = document.createElement("div")
-        divCard.setAttribute('class', 'card')
-        divCard.append(ballName, creatorName, useBtn)
-        document.querySelector("#ball-collection").append(divCard)
+            let deleteBtn = document.createElement('button')
+            deleteBtn.setAttribute('class', 'delete-ball-button')
+            deleteBtn.setAttribute('id', ball.id)
+            deleteBtn.innerText = "x"
+            deleteBtn.addEventListener("click", function(e){
+                e.target.getAtrribute("id")
+            }, {once:true})
 
-        }
+            let divCard = document.createElement("div")
+            divCard.setAttribute('class', 'card')
+            divCard.append(ballName, creatorName, useBtn, deleteBtn)
+            document.querySelector("#ball-collection").append(divCard)
+        });
     }
-   
+        
+    function deleteBall(){
+        fetch("http://localhost:3000/eightballs/${ball.id}", {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        }
+    )}
+
+    function playBall(){
+
+    }
+  
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const BASEURL = 'http://localhost:3000'
  function getBalls() {
     return fetch(`${BASEURL}/eightballs`)
     .then(resp => resp.json())
-    .then(resp => BallIndex.renderBalls(resp))     
+    .then(resp => renderBalls(resp))     
   };
 
    const allBtn = document.getElementById("all-balls");
